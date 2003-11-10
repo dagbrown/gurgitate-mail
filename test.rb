94249c500866403f74aafbb66e095fab81692068
dagbrown <dagbrown>
2003-11-10 03:03:08 +0000

    Stuck in a bunch of tests for the Headers class.  Up next: testing the gurgitator itself!

--- test.rb
+++ test.rb
@@ -17,20 +17,20 @@ class TC_Header < Test::Unit::TestCase
     # end
 
     def test_simple_header
-        h=Gurgitate::Header.new("From: dagbrown@example.com")
+        h=Gurgitate::Header.new("From: fromheader@example.com")
         assert_equal(h.name,"From", "Simple header name is From")
-        assert_equal(h.contents,"dagbrown@example.com", 
-            "Contents is dagbrown@example.com")
-        assert_equal(h.value,"dagbrown@example.com", 
-            "Contents is dagbrown@example.com")
+        assert_equal(h.contents,"fromheader@example.com", 
+            "Contents is fromheader@example.com")
+        assert_equal(h.value,"fromheader@example.com", 
+            "Contents is fromheader@example.com")
     end
 
     def test_malcapitalized_header
-        h=Gurgitate::Header.new("FROM: dagbrown@example.com")
+        h=Gurgitate::Header.new("FROM: fromheader@example.com")
         assert_equal(h.name,"From", "Badly-capitalized header is From")
-        assert_equal(h.contents,"dagbrown@example.com", 
+        assert_equal(h.contents,"fromheader@example.com", 
             "Badly-capitalized header")
-        assert_equal(h.value,"dagbrown@example.com", 
+        assert_equal(h.value,"fromheader@example.com", 
             "Badly-capitalized header")
     end
 
@@ -38,12 +38,31 @@ class TC_Header < Test::Unit::TestCase
         assert_raises(Gurgitate::IllegalHeader,"Empty name") {
             h=Gurgitate::Header.new(": Hi")
         }
-        assert_raises(Gurgitate::IllegalHeader,"Empty header contents") {
-            h=Gurgitate::Header.new("From: ")
-        }
+#         assert_raises(Gurgitate::IllegalHeader,"Empty header contents") {
+#             h=Gurgitate::Header.new("From: ")
+#         }
         assert_raises(Gurgitate::IllegalHeader,"Bad header syntax") {
             h=Gurgitate::Header.new("This is completely wrong")
         }
+    end
+    def test_extending_header
+        h=Gurgitate::Header.new("From: fromheader@example.com")
+        h << "  (Dave Brown)"
+        assert_equal(h.name,"From","Extended header is From")
+        assert_equal(h.contents,"fromheader@example.com\n  (Dave Brown)",
+            "Extended header contains all data")
+        assert_equal(h.contents,h.value,"Contents same as value")
+    end
+
+    def test_conversion_to_String
+        h=Gurgitate::Header.new("From: fromheader@example.com")
+        assert_equal(h.to_s,"From: fromheader@example.com", "Conversion to string returns input")
+    end
+
+    def test_regex_match
+        h=Gurgitate::Header.new("From: fromheader@example.com")
+        assert_equal(0,h.matches(/fromheader/),"Matches regex that would match input")
+        assert_equal(nil,h.matches(/notininput/),"Does not match regex that would not match input")
     end
 end
 
@@ -139,7 +158,7 @@ EOF
     end
 
     def test_multiline_headers
-        h=Gurgitate::Header.new(<<'EOF'
+        h=Gurgitate::Headers.new(<<'EOF'
 From fromline@example.com Sat Oct 25 12:58:31 PDT 2003
 From: fromheader@example.com
 To: toheader@example.com,
@@ -147,13 +166,13 @@ To: toheader@example.com,
 Subject: Subject line
 EOF
         )
-        assert_equal(h.from,"fromline@example.com,\n    nexttoheader@example.com")
+        assert_equal(h.from,"fromline@example.com")
         assert_equal(1,h["From"].length)
         assert_equal("From",h["From"][0].name)
         assert_equal("fromheader@example.com",h["From"][0].contents)
         assert_equal(1,h["To"].length)
         assert_equal("To",h["To"][0].name)
-        assert_equal("toheader@example.com",h["To"][0].contents)
+        assert_equal("toheader@example.com,\n    nexttoheader@example.com",h["To"][0].contents)
         assert_equal(1,h["Subject"].length)
         assert_equal("Subject",h["Subject"][0].name)
         assert_equal("Subject line",h["Subject"][0].contents)
