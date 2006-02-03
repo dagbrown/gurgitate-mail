bf793529b7261322c460ed54af015471a4b9737c
dagbrown <dagbrown>
2006-02-03 05:03:07 +0000

    Made it check that the system rules files are owned by root (sorry, Windows users) and that the non-system rules files are owned by the user.

--- gurgitate.rb
+++ gurgitate.rb
@@ -7,4 +7,6 @@ require "gurgitate-mail"
 # change this on installation to taste
 GLOBAL_RULES="/etc/gurgitate-rules"
 
-Gurgitate::Gurgitate.new(STDIN).process(GLOBAL_RULES,true).process
+Gurgitate::Gurgitate.new(STDIN) .
+    process(GLOBAL_RULES,:chain => true, :system => true) .
+    process
