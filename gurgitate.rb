c7cbed237ecf63de314ea30fa9cf53feae8cff00
aredridel <aredridel>
2006-02-03 07:59:02 +0000

    - Add post-user system rules - Fix ownership check on system rules files - Some error message cleanup

--- gurgitate.rb
+++ gurgitate.rb
@@ -6,7 +6,9 @@ require "gurgitate-mail"
 
 # change this on installation to taste
 GLOBAL_RULES="/etc/gurgitate-rules"
+GLOBAL_RULES_POST="/etc/gurgitate-rules-default"
 
 Gurgitate::Gurgitate.new(STDIN) .
     process(GLOBAL_RULES,:chain => true, :system => true) .
-    process
+    process(:default, :chain => true) .
+    process(GLOBAL_RULES_POST, :system => true)
