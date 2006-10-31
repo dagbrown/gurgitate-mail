659ac944c8891a424f9318381ad9ecd7f962e686
dagbrown <dagbrown>
2006-10-31 05:09:16 +0000

    Added things to let you specify sender and recipients on the command line. This could come in handy later.

--- gurgitate.rb
+++ gurgitate.rb
@@ -10,20 +10,26 @@ GLOBAL_RULES="/etc/gurgitate-rules"
 GLOBAL_RULES_POST="/etc/gurgitate-rules-default"
 
 commandline_files = []
+mail_sender = nil
 
 opts = OptionParser.new do |o|
-  o.on("-f FILE", "--file FILE", "Use FILE as a rules file") do |file|
+  o.on "-f FILE", "--file FILE", "Use FILE as a rules file" do |file|
     commandline_files << file
   end
-  o.on_tail("-h", "--help", "Show this message") do
+
+  o.on "-s SENDER", "--sender SENDER", "Use SENDER as sender" do |sender|
+      mail_sender = sender
+  end
+
+  o.on_tail "-h", "--help", "Show this message" do
     puts opts
     exit
   end
 end
 
-opts.parse!(ARGV)
+mail_recipients = opts.parse(ARGV)
 
-gurgitate = Gurgitate::Gurgitate.new(STDIN)
+gurgitate = Gurgitate::Gurgitate.new(STDIN, mail_recipients, mail_sender)
 
 if commandline_files.length > 0
   commandline_files.each do |file|
