a70bfcec87aaccfa254bad04f64d40119aca5b69
dagbrown <dagbrown>
2006-10-31 06:12:50 +0000

    Split the tests out into their own little files.

---
+++ tests/test_process.rb
@@ -0,0 +1,19 @@
+require 'test/unit'
+require 'test/unit/ui/console/testrunner'
+require 'stringio'
+
+class TC_Process < Test::Unit::TestCase
+	def setup
+		m = StringIO.new("From: me\nTo: you\nSubject: test\n\nHi.")
+		@gurgitate = Gurgitate::Gurgitate.new(m)
+	end
+	
+	def test_1
+		assert_nothing_raised do
+			@gurgitate.process { break }
+			@gurgitate.process { pipe('cat > /dev/null') }
+			@gurgitate.process { return }
+		end
+	end
+end
+
