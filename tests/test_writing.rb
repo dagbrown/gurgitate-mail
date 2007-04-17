0a977cc19f8fe3059f89b1c0a8cc72fccf1942bc
Dave Brown <dagbrown@aerogel.lart.ca>
2007-04-17 09:33:03 +0900

    Added some basic message-creation tests.

--- tests/test_writing.rb
+++ tests/test_writing.rb
@@ -6,9 +6,22 @@ require 'stringio'
 
 class TC_Writing < Test::Unit::TestCase
     def test_null_message
-        mess = nil
         assert_nothing_raised do
-            mess = Gurgitate::Mailmessage.new
+            Gurgitate::Mailmessage.new
         end
+    end
+
+    def test_one_header_message
+        mess = Gurgitate::Mailmessage.new
+        mess.headers["From"] = "test@test"
+        assert_equal "From: test@test",  mess.headers.to_s
+        assert_equal "From: test@test\n\n", mess.to_s
+    end
+
+    def test_two_headers_message
+        mess = Gurgitate::Mailmessage.new
+        mess.headers["From"] = "test@test"
+        mess.headers["To"]   = "test2@test2"
+        assert_equal "From: test@test\nTo: test2@test2",  mess.headers.to_s
     end
 end
