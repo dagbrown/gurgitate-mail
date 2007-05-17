58b4b37c322fb71725cbef568b76e0ead135863e
Dave Brown <dagbrown@aerogel.lart.ca>
2007-05-18 08:57:42 +0900

    Added a bunch of tests for adding gurgitate rules.

--- test/test_rules.rb
+++ test/test_rules.rb
@@ -1,43 +1,73 @@
 require "test/gurgitate-test"
+require "etc"
 
 class TC_Rules < GurgitateTest
-    def test_add_rules_normal
-        rulesfile = File.join(@testdir, "rules_1.rb")
+    def setup
+        super
+        @rulesfile = File.join(@testdir, "rules_1.rb")
         File.open(rulesfile, File::CREAT|File::WRONLY) do |f|
             f.puts "nil"
         end
+        
+        @defaultrules = File.join(@testdir, ".gurgitate-rules.rb")
+        File.open File.join(@defaultrules), File::CREAT|File::WRONLY do |f|
+            f.puts "nil"
+        end
+    end
 
+    attr_reader :rulesfile
+
+    def teardown
+        File.unlink rulesfile if File.exist? rulesfile
+    end
+
+    def test_add_rules_normal
         assert @gurgitate.add_rules(rulesfile)
 
         assert_equal [rulesfile], @gurgitate.instance_variable_get("@rules")
     end
 
     def test_add_unreadable_rules
-        rulesfile = File.join(@testdir, "rules_1.rb")
-        File.open(rulesfile, File::CREAT|File::WRONLY) do |f|
-            f.puts "nil"
-        end
-
         FileUtils.chmod 0, rulesfile
 
         assert_equal false, @gurgitate.add_rules(rulesfile)
     end
 
     def test_add_nonexistent_rules
-        rulesfile = File.join(@testdir, "rules_1.rb")
+        File.unlink rulesfile
         assert_equal false, @gurgitate.add_rules(rulesfile)
     end
 
     def test_add_default_rules
-        File.open File.join(@testdir,".gurgitate-rules.rb"), File::CREAT|File::WRONLY do |f|
-            f.puts "nil"
-        end
-
         assert_nothing_raised do
             @gurgitate.add_rules :default
         end
 
-        assert_equal [File.join(@testdir,".gurgitate-rules.rb")],
+        assert_equal [@defaultrules],
             @gurgitate.instance_variable_get("@rules")
+    end
+
+    def test_add_system_rules
+        unless Etc.getpwuid.uid == 0
+            assert_equal false, @gurgitate.add_rules(rulesfile, :system => true)
+        else
+            # but really, fer crying out loud, DON'T RUN TESTS AS ROOT
+            assert true, @gurgitate.add_rules(rulesfile, :system => true)
+        end
+    end
+
+    def test_add_user_rules
+        assert @gurgitate.add_rules(rulesfile, :user => true)
+    end
+
+    def test_add_user_rules_file_not_found
+        File.unlink rulesfile
+        assert_equal false, @gurgitate.add_rules(rulesfile, :user => true)
+    end
+
+    def test_bad_syntax
+        assert_raises ArgumentError do
+            @gurgitate.add_rules(rulesfile, :honk)
+        end
     end
 end
