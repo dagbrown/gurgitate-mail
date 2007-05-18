34dbea320de90bbc2ce78b284807fe41b085d801
Dave Brown <dagbrown@aerogel.lart.ca>
2007-05-18 09:06:08 +0900

    Started added tests for PROCESSING rules

--- test/test_rules.rb
+++ test/test_rules.rb
@@ -71,3 +71,37 @@ class TC_Rules < GurgitateTest
         end
     end
 end
+
+class TC_ExecuteRules < TC_Rules
+    def setup
+        super
+        @invalidrules = File.join(@testdir, "rules_invalid.rb")
+        File.open @invalidrules, "w" do |f|
+            f.puts "invalid syntax"
+        end
+        @exceptionrules = File.join(@testdir, "rules_exception.rb")
+        File.open @exceptionrules, "w" do |f|
+            f.puts "raise RuntimeError, 'testing'"
+        end
+    end
+
+    def teardown
+        File.unlink @invalidrules if File.exists? @invalidrules
+    end
+
+    def test_process_default
+        @gurgitate.add_rules :default
+        assert_nothing_raised do
+            @gurgitate.process
+        end
+    end
+
+    def test_process_rules_not_found
+        @gurgitate.add_rules @rulesfile
+        File.unlink @rulesfile
+        assert_nothing_raised do 
+            @gurgitate.process
+        end
+        assert File.exists?(@spoolfile)
+    end
+end
