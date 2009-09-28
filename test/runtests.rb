881fd09f2f715b13501c9feb856e9f2deb38c8bb
Dave Brown <dagbrown@lart.ca>
2009-09-29 04:56:08 +0900

    Changed the way tests work.

--- test/runtests.rb
+++ test/runtests.rb
@@ -1,5 +1,12 @@
 require 'test/unit'
-require 'test/unit/ui/console/testrunner'
+tester = nil
+begin
+    require 'test/unit/ui/console/testrunner'
+    tester = Test::Unit::UI::Console::TestRunner
+rescue LoadError  # ruby 1.9
+    require 'test/unit'
+    tester = Test::Unit
+end
 require 'stringio'
 require 'pathname'
 
